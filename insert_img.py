c4cdd7fd1773d162ab6c7f8a2b418981011f7189
linx02 <elvius.linus@gmail.com>
2024-05-06 14:40:13 +0200

    Insert images

---
+++ insert_img.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+with open('links.txt', 'r') as file:
+    links = file.readlines()
+
+ids = [link.replace('https://amazon.com/dp/', '') for link in links]
+ids = [link.replace('\n', '') for link in ids]
+files = [link + '.txt' for link in ids]
+
+for i, f in enumerate(files):
+
+    print(f)
+
+    img_tag = f'<img src="{data[i]["product_image_url"]}" alt="Product image" />'
+
+    with open(f'reviews/{f}', 'r') as file:
+        content = file.read()
+    
+    content = img_tag + r'\n' + content
+
+    with open(f'reviews/{f}', 'w') as file:
+        file.write(content)
+    
+    print(f'Inserted image in {f}')
